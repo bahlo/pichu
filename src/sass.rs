9e2efb6bf856a4e3a31d958b28c831c44a33ebbf
Arne Bahlo <hey@arne.me>
2025-06-07 12:29:37 +0200

    Add test for write, match fs::write signature

--- src/sass.rs
+++ src/sass.rs
@@ -5,10 +5,7 @@ use crate::{write, Error};
 /// Render a SASS/SCSS file to the destination.
 /// Other SASS/SCSS files next to the provided one will be available for
 /// inclusion.
-pub fn render_sass<A: AsRef<Path>>(
-    source: impl AsRef<Path>,
-    destination: A,
-) -> Result<String, Error> {
+pub fn render_sass(source: impl AsRef<Path>, path: impl AsRef<Path>) -> Result<String, Error> {
     let source = source.as_ref();
     let options = match source.parent() {
         Some(parent) => grass::Options::default().load_path(parent),
@@ -20,6 +17,6 @@ pub fn render_sass<A: AsRef<Path>>(
         .chars()
         .take(16)
         .collect();
-    write(css, destination)?;
+    write(path, css)?;
     Ok(hash)
 }
